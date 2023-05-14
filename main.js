699bdda0df966c31e1731b0503be11767c986271
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2023-05-14 08:11:33 -0500

    add tailwindcss

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
+  <div class="px-0">
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="px-0" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      <img src="${javascriptLogo}" class=" px-0" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1 class="bg-black text-yellow-200">Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
+    <p class="text-blue-600">
       Click on the Vite logo to learn more
     </p>
   </div>
